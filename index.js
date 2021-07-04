8707272973496d3df131696cdefa63cf41f0bfe0
Francisco Monterroso <sistemasooliva@gmail.com>
2021-07-04 17:06:36 -0600

    Agregando package json y configuraciones iniciales

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-Prueba
+const express = require('express');
+var app = express();
+
+// parse application/json, basically parse incoming Request Object as a JSON Object 
+app.use(express.json());
+// parse incoming Request Object if object, with nested objects, or generally any type.
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/test', (req, res) => {
+    console.log(req.body);
+    res.status(200).send("Hello world!");
+});
+
+
+app.listen('4000', () => {
+    console.log(`Servidor iniciado en el puerto: 4000`);
+});
